89d295b8cf09230d70d58c8d1f404b1523efb22b
Szabó Gábor <szabogabor@hydrogene.hu>
2017-02-21 16:14:49 +0100

    start

---
+++ front-page.php
@@ -0,0 +1,207 @@
+<?php while (have_posts()) : the_post(); ?>
+
+<section class="whatwedo ps" id="whatwedo">
+  <div class="row">
+    <div class="columns">
+      <h2 class="floatingtitle">What We Do</h2>
+      <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem assumenda perferendis nostrum velit eaque, quis, ea ex aliquam sequi, obcaecati voluptate possimus non voluptas accusamus quod corrupti fugit, inventore minus!</p>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Curabitur blandit tempus porttitor. Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.</p>
+      <a href="#" class="button secondary">Secondary</a>
+      <a href="#" class="button warning">Warning</a>
+      <a href="#" class="button alert">Alert</a>
+      <a href="#" class="button success">Succes</a>
+      <a href="#" class="button">Primary</a>
+    </div>
+  </div>
+</section>
+
+<section class="ourservices ps" id="ourservices">
+  <div class="row">
+    <div class="columns">
+      <h2 class="floatingtitle">Our Services</h2>
+      <p class="lead">Dolor sit amet, consectetur adipisicing elit. Ratione? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem assumenda perferendis nostrum velit eaque, quis, ea ex aliquam sequi, obcaecati voluptate possimus non voluptas accusamus quod corrupti fugit, inventore minus!</p>
+      <section class="service ps ps--narrow">
+        <h3 class="accentttile">Pragmatic Leadership Training</h3>
+        <h4>For Whom</h4>
+        <p>For leaders who understand that true leadership is not based on authority and "knowing better", but creating an emphatic and inspiring atmosphere that helps others grow and feel meaningful.</p>
+        <h4>What do you get?</h4>
+        <p>A six-day, practice-oriented, small-group training, focused attention from two of our professionals, and a comforatble atmosphere with supervison after the program.</p>
+        <h4>What will be the result?</h4>
+        <p>Specific personal development at the field of choice.Highly improved soft skills. Emerging inner values. Transparent communication and coaching skills. New habit of choice. A slightly better life.</p>
+      </section>
+      <section class="service ps ps--narrow">
+        <h3 class="accentttile">Transparent Communication</h3>
+        <h4>For Whom</h4>
+        <p>For teams and organisations that are bored of the classic teambuilding processes and look for a brief and effective method of making a team better.</p>
+        <h4>What do you get?</h4>
+        <p>A two-time 3 hours long coaching session each followed by a 2 month follow up period.</p>
+        <h4>What will be the result?</h4>
+        <p>A more effective team. Significant improvement in communication and teamwork. Personal development processes for each member related to the common goal of the team.</p>
+      </section>
+      <section class="service ps ps--narrow">
+        <h3 class="accentttile">Mind Management</h3>
+        <h4>For Whom</h4>
+        <p>For managers and executives who want true personal transformation guided by a coach and supported by stakeholders, with scaleable results and a change in the personal narrative as well.</p>
+        <h4>What do you get?</h4>
+        <p>A 6-month long transformation process with monthly sessions, customized exercies and constant follow up by stakeholders.</p>
+        <h4>What will be the result?</h4>
+        <p>A leader living up to his inner potential.</p>
+      </section>
+    </div>
+  </div>
+</section>
+
+<section class="communication ps" id="communication">
+
+      <div class="row">
+        <div class="columns tablet-8 tablet-centered large-6">
+            <div class="callout secondary">
+              <h3>Get Our Brochure in Your Inbox</h3>
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolor sit amet, consectetur adipisicing.</p>
+              <select name="" id="">
+                <option value="0">Select service</option>
+                <option value="1">Pragmatic Leadership Training</option>
+                <option value="2">Transparent Communication</option>
+                <option value="3">Mind Management</option>
+                <option value="4">All services</option>
+              </select>
+              <input type="email" placeholder="E-mail address">
+              <a href="#" class="button hollow expanded">Send Me the Brochure!</a>
+          </div>
+        </div>
+      </div>
+</section>
+
+<section class="ourvalues ps" id="ourvalues">
+  <div class="row">
+    <div class="columns">
+      <h2 class="floatingtitle">Our Values</h2>
+      <p class="lead">Dolor sit amet, consectetur adipisicing elit. Ratione? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem assumenda perferendis nostrum velit eaque, quis, ea ex aliquam sequi, obcaecati voluptate possimus non voluptas accusamus quod corrupti fugit, inventore minus!</p>
+      <div class="row small-up-2 medium-up-2">
+          <div class="columns">
+            <div class="valuecard">
+              <img class="value__icon" src="http://placehold.it/60x60" alt="Lorem ipsum">
+              <h3 class="value__name">Sen</h3>
+              <p class="value__descr">Maecenas faucibus mollis interdum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Maecenas sed diam eget risus varius blandit sit amet non magna. Vestibulum id ligula porta felis euismod semper. Nullam quis risus eget urna mollis ornare vel eu leo. Maecenas faucibus mollis interdum. Maecenas faucibus mollis interdum.</p>
+            </div>
+          </div>
+          <div class="columns">
+            <div class="valuecard">
+              <img class="value__icon" src="http://placehold.it/60x60" alt="Lorem ipsum">
+              <h3 class="value__name">Too</h3>
+              <p class="value__descr">Donec sed odio dui. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam id dolor id nibh ultricies vehicula ut id elit. Integer posuere erat a ante venenatis dapibus posuere velit aliquet.</p>
+            </div>
+          </div>
+          <div class="columns">
+            <div class="valuecard">
+              <img class="value__icon" src="http://placehold.it/60x60" alt="Lorem ipsum">
+              <h3 class="value__name">Pragma</h3>
+              <p class="value__descr">Porro placeat, enim blanditiis repellendus at temporibus perferendis et voluptates iusto quo exercitationem, repudiandae officiis ipsa quibusdam perspiciatis magni delectus, illum sapiente! Lorem ipsum dolor sit amet.</p>
+            </div>
+          </div>
+          <div class="columns">
+            <div class="valuecard">
+              <img class="value__icon" src="http://placehold.it/60x60" alt="Lorem ipsum">
+              <h3 class="value__name">Ashma</h3>
+              <p class="value__descr">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro placeat, enim blanditiis repellendus at temporibus perferendis et voluptates iusto quo exercitationem, repudiandae officiis ipsa quibusdam perspiciatis magni delectus, illum sapiente!</p>
+            </div>
+          </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="aboutus ps" id="aboutus">
+  <div class="row">
+    <div class="columns">
+      <h2 class="floatingtitle">Who We Are</h2>
+      <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem assumenda perferendis nostrum velit eaque, quis, ea ex aliquam sequi, obcaecati voluptate possimus non voluptas accusamus quod corrupti fugit, inventore minus!</p>
+      <div class="row">
+        <div class="columns small-6">
+          <div class="membercard">
+            <figure class="membercard__fig">
+              <img src="http://placehold.it/768x768" alt="">
+            </figure>
+            <h3 class="membercard__name">Gipsz Jakab Zoltán</h3>
+            <p class="membercard__role">Chief Officer</p>
+            <p class="membercard__bio">Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Nulla vitae elit libero, a pharetra augue. Donec id elit non mi porta gravida at eget metus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec sed odio dui.</p>
+          </div>
+        </div>
+        <div class="columns small-6">
+          <div class="membercard">
+            <figure class="membercard__fig">
+              <img src="http://placehold.it/768x768" alt="">
+            </figure>
+            <h3 class="membercard__name">László Gipsz Nyakas</h3>
+            <p class="membercard__role">Manager</p>
+            <p class="membercard__bio">Lacus vel augue laoreet rutrum faucibus dolor auctor. Nulla vitae elit libero, a pharetra augue. Donec id elit non mi porta gravida at eget metus. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec sed odio dui.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+
+
+
+
+<section class="whoweworkwith ps" id="whoweworkwith">
+  <div class="row">
+    <div class="columns">
+    <h2 class="flaotingtitle">Who We work With</h2>
+    <p class="lead">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem assumenda perferendis nostrum velit eaque, quis, ea ex aliquam sequi, obcaecati voluptate possimus non voluptas accusamus quod corrupti fugit, inventore minus!</p>
+    <div class="row">
+      <div class="columns xlarge-10 xlarge-centered">
+        <div class="row small-up-2 medium-up-3">
+          <div class="columns">
+            <div class="clientcard">
+              <a href="#"><img src="http://placehold.it/768x768" alt="Lorem ipsum"></a>
+            </div>
+          </div>
+          <div class="columns">
+            <div class="clientcard">
+              <a href="#"><img src="http://placehold.it/768x768" alt="Lorem ipsum"></a>
+            </div>
+          </div>
+          <div class="columns">
+            <div class="clientcard">
+              <a href="#"><img src="http://placehold.it/768x768" alt="Lorem ipsum"></a>
+            </div>
+          </div>
+          <div class="columns">
+            <div class="clientcard">
+              <a href="#"><img src="http://placehold.it/768x768" alt="Lorem ipsum"></a>
+            </div>
+          </div>
+          <div class="columns">
+            <div class="clientcard">
+              <a href="#"><img src="http://placehold.it/768x768" alt="Lorem ipsum"></a>
+            </div>
+          </div>
+          <div class="columns">
+            <div class="clientcard">
+              <a href="#"><img src="http://placehold.it/768x768" alt="Lorem ipsum"></a>
+            </div>
+          </div>
+          <div class="columns">
+            <div class="clientcard">
+              <a href="#"><img src="http://placehold.it/768x768" alt="Lorem ipsum"></a>
+            </div>
+          </div>
+          <div class="columns">
+            <div class="clientcard">
+              <a href="#"><img src="http://placehold.it/768x768" alt="Lorem ipsum"></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</section>
+
+
+
+
+<?php endwhile; ?>
+
--- templates/footer.php
+++ templates/footer.php
@@ -1,5 +1,13 @@
-<footer class="content-info">
-  <div class="container">
-    <?php dynamic_sidebar('sidebar-footer'); ?>
+<footer class="ps sitefooter">
+  <div class="row">
+    <div class="columns text-center">
+      <p>&copy; <?= date(Y) ?> FROMINGMINDS &middot;  All rights reserved</p>
+
+    </div>
+  </div>
+  <div class="row">
+    <div class="columns">
+          <?php dynamic_sidebar('sidebar-footer'); ?>
+    </div>
   </div>
 </footer>
